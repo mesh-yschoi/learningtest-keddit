ecb35e62f155ad83ec9de80b1e23720589130cc6
최윤석 <yunseok.choi@woowahan.com>
2018-09-26 17:54:26 +0900

    updating part6

--- app/src/main/java/moerspace/learningtest/keddit/features/news/NewsManager.kt
+++ app/src/main/java/moerspace/learningtest/keddit/features/news/NewsManager.kt
@@ -1,22 +1,32 @@
 package moerspace.learningtest.keddit.features.news
 
 import io.reactivex.Single
+import moerspace.learningtest.keddit.api.RestAPI
 import moerspace.learningtest.keddit.commons.RedditNewsItem
 
-class NewsManager {
+class NewsManager(private val api: RestAPI = RestAPI()) {
 
-    fun getNews(): Single<List<RedditNewsItem>> {
+    fun getNews(limit: String = "10"): Single<List<RedditNewsItem>> {
         return Single.create { emitter ->
-            val news = (1..10).map { RedditNewsItem(
-                    "author$it",
-                    "Title $it",
-                    it,
-                    1457207701L - it * 200,
-                    "https://picsum.photos/200/200?image=$it",
-                    "url"
-            ) }
-
-            emitter.onSuccess(news)
+            val callResponse = api.getNews("", limit)
+            val response = callResponse.execute()
+            val redditNewsResponse = response.body()
+            if (response.isSuccessful && redditNewsResponse != null) {
+                val news = redditNewsResponse.data.children.map {
+                    val item = it.data
+                    RedditNewsItem(
+                        item.author,
+                        item.title,
+                        item.num_comments,
+                        item.created,
+                        item.thumbnail,
+                        item.url
+                    )
+                }
+                emitter.onSuccess(news)
+            } else {
+                emitter.onError(Throwable(response.message()))
+            }
         }
     }
 }
